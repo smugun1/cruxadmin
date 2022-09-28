51b5b3085c33a0210430cc9d26695d53f6f8484f
smugun2013@hotmail.com <smugun2013@gmail.com>
2022-09-28 21:07:42 +0300

    Added signup,signin, logout .env and requirements file

--- crux/crux/settings.py
+++ crux/crux/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from environ import Env
 
+env = Env()
+env.read_env()  # read .env file, if it exists
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#0vcb0)vno%h)nc%2u16*@lv-4c!74e8y=7uizwehy5zd-i7d4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
